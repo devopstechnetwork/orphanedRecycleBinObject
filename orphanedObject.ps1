767fdb9b1a2e06baf9cfdaefcacf5c09a4d9fa4d
Lucius Bono <luciusbono@gmail.com>
2014-01-23 15:06:59 -0800

    Initial file upload

---
+++ orphanedObject.ps1
@@ -0,0 +1,3 @@
+﻿param($orphanedObject)
+$objectGUID = ('*'+((Get-ADUser $orphanedObject).ObjectGUID)+'*')
+get-adgroup -filter {samaccountname -like $objectGUID} -SearchBase "OU=NetIQRecycleBin,DC=dolby,DC=net"
